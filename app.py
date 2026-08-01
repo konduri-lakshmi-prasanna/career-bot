7909b75936c3d8a71670ead0a29ed448a8b34639
konduri-lakshmi-prasanna <konduriprasanna22@gmail.com>
2026-08-01 17:57:58 +0530

    testing for changes

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.set_page_config(page_title="CareerBot")
 
+st.success("Before import")
+
 from ui.styles import get_custom_css
 
-st.markdown(get_custom_css(), unsafe_allow_html=True)
-
-st.success("Styles Loaded")
+st.success("After import")
